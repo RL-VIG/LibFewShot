fc589200dea3aed44e736eb951316b82d43ce76d
WangZiyi <wangziyi@smail.nju.edu.cn>
2020-12-28 15:14:04 +0800

    fix collate_fn again

--- core/data/collates/collate_fns.py
+++ core/data/collates/collate_fns.py
@@ -1,5 +1,7 @@
 import itertools
 import torch
+import unittest
+
 
 class VanillaCollateFn(object):
     """
@@ -13,11 +15,14 @@ class VanillaCollateFn(object):
     def method(self, batch):
         try:
             query_images, query_targets, support_images, support_targets = batch[0]
-            query_images = [self.trfms(image).unsqueeze(0) for image in query_images]
+            query_images = [self.trfms(image).unsqueeze(0)
+                            for image in query_images]
+            query_targets = [torch.tensor(target) for target in query_targets]
             # unsqueeze: n_dim=3 -> n_dim=4，如果使用原来的方法，dataloader应该返回的是n_dim=4
-            support_images = [self.trfms(image).unsqueeze(0) for image in support_images]
-            query_targets = [torch.tensor([target]) for target in query_targets]
-            support_targets = [torch.tensor([target]) for target in support_targets]
+            support_images = [self.trfms(image).unsqueeze(0)
+                              for image in support_images]
+            support_targets = [torch.tensor([target])
+                               for target in support_targets]
             return query_images, query_targets, support_images, support_targets
         except TypeError:
             raise TypeError('不应该在dataset传入transform，在collate_fn传入transform')
@@ -32,6 +37,7 @@ class SampleFiveTimesAugCollateFn(object):
     """
     增广5次的样例: 01234 -> 0000011111222223333344444
     """
+
     def __init__(self, trfms):
         super(SampleFiveTimesAugCollateFn, self).__init__()
         self.trfms = trfms
@@ -41,11 +47,21 @@ class SampleFiveTimesAugCollateFn(object):
     def method(self, batch):
         try:
             query_images, query_targets, support_images, support_targets = batch[0]
-            query_images = [self.trfms(image).unsqueeze(0) for image in query_images]
+            query_images = [self.trfms(image).unsqueeze(0)
+                            for image in query_images]
+            query_targets = [torch.tensor([target])
+                             for target in query_targets]
             # Do aug
-            aug_support_images = list(itertools.chain.from_iterable([[image]*self.times for image in support_images]))
-            aug_support_images = [self.trfms(image).unsqueeze(0) for image in support_images]
-            aug_support_targets = list(itertools.chain.from_iterable([[target]*self.times for target in support_targets]))
+            aug_support_images = list(itertools.chain.from_iterable(
+                [[image]*self.times for image in support_images]))
+            aug_support_images = [self.trfms(image).unsqueeze(
+                0) for image in aug_support_images]
+            aug_support_targets = list(itertools.chain.from_iterable(
+                [[target]*self.times for target in support_targets]))
+            aug_support_targets = [torch.tensor(
+                [target]) for target in aug_support_targets]
+            unittest.TestCase.assertEqual(
+                len(aug_support_images), len(aug_support_targets))
             return query_images, query_targets, aug_support_images, aug_support_targets
         except TypeError:
             raise TypeError('不应该在dataset传入transform，在collate_fn传入transform')
