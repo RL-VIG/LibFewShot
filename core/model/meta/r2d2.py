a3673af1ffe3311c45e322956719966380848583
yangcedrus <yangcedrus@gmail.com>
2021-01-11 11:28:49 +0800

    support r2d2 muti-task

--- core/model/meta/r2d2.py
+++ core/model/meta/r2d2.py
@@ -64,18 +64,17 @@ class R2D2Head(nn.Module):
     https://arxiv.org/abs/1805.08136
     """
 
-    def __init__(self, ratio=50.0):
+    def __init__(self):
         super(R2D2Head, self).__init__()
-        self.ratio = ratio
 
-    def forward(self, query, support, support_labels, n_way, n_shot):
+    def forward(self, query, support, support_labels, n_way, n_shot, ratio=50.0):
         tasks_per_batch = query.size(0)
         n_support = support.size(1)
 
-        assert (query.dim() == 3)
-        assert (support.dim() == 3)
-        assert (query.size(0) == support.size(0) and query.size(2) == support.size(2))
-        assert (n_support == n_way * n_shot)  # n_support must equal to n_way * n_shot
+        # assert (query.dim() == 3)
+        # assert (support.dim() == 3)
+        # assert (query.size(0) == support.size(0) and query.size(2) == support.size(2))
+        # assert (n_support == n_way * n_shot)  # n_support must equal to n_way * n_shot
 
         support_labels_one_hot = one_hot(support_labels.view(tasks_per_batch * n_support), n_way)
         support_labels_one_hot = support_labels_one_hot.view(tasks_per_batch, n_support, n_way)
@@ -84,7 +83,7 @@ class R2D2Head(nn.Module):
 
         # Compute the dual form solution of the ridge regression.
         # W = X^T(X X^T - lambda * I)^(-1) Y
-        ridge_sol = computeGramMatrix(support, support) + self.ratio * id_matrix
+        ridge_sol = computeGramMatrix(support, support) + ratio * id_matrix
         ridge_sol = binv(ridge_sol)
         ridge_sol = torch.bmm(support.transpose(1, 2), ridge_sol)
         ridge_sol = torch.bmm(ridge_sol, support_labels_one_hot)
@@ -97,27 +96,18 @@ class R2D2Head(nn.Module):
 
 
 class R2D2(MetaModel):
-    def __init__(self, way_num, shot_num, query_num, feature, device):
+    def __init__(self, way_num, shot_num, query_num, feature, device, inner_optim=None, inner_train_iter=10):
         super(R2D2, self).__init__(way_num, shot_num, query_num, feature, device)
+        self.register_parameter('alpha', nn.Parameter(torch.tensor([1.])))
+        self.register_parameter('beta', nn.Parameter(torch.tensor([0.])))
+        self.register_parameter('gamma', nn.Parameter(torch.tensor([50.])))
         self.loss_func = nn.CrossEntropyLoss()
         self.classifier = R2D2Head()
+        self.inner_optim = inner_optim
+        self.inner_train_iter = inner_train_iter
         self._init_network()
 
     def set_forward(self, batch, ):
-        support_images, support_targets, query_images, query_targets = \
-            self.progress_batch(batch)
-
-        with torch.no_grad():
-            support_feat = self.model_func(support_images)
-        classifier_copy = self.train_loop(support_feat, support_targets)
-
-        query_feat = self.model_func(query_images)
-        output = classifier_copy(query_feat)
-        prec1, _ = accuracy(output, query_targets, topk=(1, 3))
-
-        return output, prec1
-
-    def set_forward_loss(self, batch, ):
         support_images, support_targets, query_images, query_targets = \
             self.progress_batch(batch)
 
@@ -128,16 +118,62 @@ class R2D2(MetaModel):
         emb_query = emb_query.unsqueeze(0)
         emb_support = emb_support.unsqueeze(0)
 
-        output = self.classifier(emb_query, emb_support, support_targets, self.way_num,
-                                 self.shot_num)
+        output = self.classifier(emb_query, emb_support, support_targets, self.way_num, self.shot_num, self.gamma)
+        output = self.alpha * output + self.beta
+        prec1, _ = accuracy(output.squeeze(), query_targets, topk=(1, 3))
 
-        output = output.squeeze()
-        loss = self.loss_func(output, query_targets)
-        prec1, _ = accuracy(output, query_targets, topk=(1, 3))
+        return output, prec1
 
+    def set_forward_loss(self, batch, ):
+        # support_images, support_targets, query_images, query_targets = \
+        #     self.progress_batch(batch)
+
+        images, _ = batch
+        b, c, h, w = images.size()
+        episode = b // (self.way_num * (self.shot_num + self.query_num))
+        local_target = self._generate_local_targets(episode)
+
+        images = images.to(self.device)
+        local_target = local_target.to(self.device)
+
+        # emb_feat = self.model_func(images)
+        images = images.contiguous().view(episode, self.way_num, self.shot_num + self.query_num, c, h, w)
+        local_target = local_target.contiguous().view(episode, self.way_num, -1)
+
+        loss_list = []
+        prec1_list = []
+        output_list = []
+        for i in range(episode):
+            episode_images = images[i:i + 1, :]
+            episode_targets = local_target[i:i + 1, :]
+
+            support_images = episode_images[:, :, :self.shot_num, :, :, :].contiguous().view(-1, c, h, w)
+            query_images = episode_images[:, :, self.shot_num:, :, :, :].contiguous().view(-1, c, h, w)
+            support_targets = episode_targets[:, :, :self.shot_num].contiguous().view(-1)
+            query_targets = episode_targets[:, :, self.shot_num:].contiguous().view(-1)
+
+            emb_support = self.model_func(support_images)
+            emb_query = self.model_func(query_images)
+
+            # FIXME 换一种写法
+            emb_support = emb_support.unsqueeze(0)
+            emb_query = emb_query.unsqueeze(0)
+
+            output = self.classifier(emb_query, emb_support, support_targets, self.way_num, self.shot_num, self.gamma)
+            output = self.alpha * output + self.beta
+            loss = self.loss_func(output.squeeze(), query_targets)
+            prec1, _ = accuracy(output.squeeze(), query_targets, topk=(1, 3))
+
+            loss_list.append(loss)
+            prec1_list.append(prec1)
+
+        # FIXME 怎么计算多任务loss和prec1
+        loss = torch.mean(torch.stack(loss_list))
+        prec1 = torch.mean(torch.tensor(prec1_list))
+        output = torch.tensor(output_list)
         return output, prec1, loss
 
-    def train_loop(self, support_feat, support_targets):
+    def train_loop(self, emb_support, support_targets):
         raise NotImplementedError
 
     def test_loop(self, *args, **kwargs):
