62ddf62ae43764e899dfe748adebaabb21d1bc24
LegenDong <dcq2254142563@gmail.com>
2021-01-12 20:29:34 +0800

    update proto_net to support for multi-task and multi-gpu setting

--- core/model/metric/proto_net.py
+++ core/model/metric/proto_net.py
@@ -5,9 +5,34 @@ from core.utils import accuracy
 from .metric_model import MetricModel
 
 
+class ProtoLayer(nn.Module):
+    def __init__(self, way_num, shot_num, query_num):
+        super(ProtoLayer, self).__init__()
+        self.way_num = way_num
+        self.shot_num = shot_num
+        self.query_num = query_num
+
+    def forward(self, query_feat, support_feat):
+        t, wq, c = query_feat.size()
+        _, ws, _ = support_feat.size()
+
+        # t, wq, c
+        query_feat = query_feat.view(t, self.way_num * self.query_num, c)
+        # t, w, c
+        support_feat = support_feat.view(t, self.way_num, self.shot_num, c)
+        proto_feat = torch.mean(support_feat, dim=2)
+
+        # t, wq, 1, c - t, 1, w, c -> t, wq, w
+        dist = -torch.sum(torch.pow(query_feat.unsqueeze(2) -
+                                    proto_feat.unsqueeze(1), 2), dim=3)
+
+        return dist
+
+
 class ProtoNet(MetricModel):
     def __init__(self, way_num, shot_num, query_num, model_func, device):
         super(ProtoNet, self).__init__(way_num, shot_num, query_num, model_func, device)
+        self.proto_layer = ProtoLayer(way_num, shot_num, query_num)
         self.loss_func = nn.CrossEntropyLoss()
         self._init_network()
 
@@ -17,15 +42,25 @@ class ProtoNet(MetricModel):
         :param batch:
         :return:
         """
-        support_images, support_targets, query_images, query_targets = \
-            self.progress_batch(batch)
+        images, targets = self.progress_batch2(batch)
+        episode_size = images.size(0) // (self.way_num * (self.shot_num + self.query_num))
 
-        query_feat = self.model_func(query_images)
-        support_feat = self.model_func(support_images)
-        proto_feat = torch.mean(support_feat.view(self.way_num, self.shot_num, -1), 1)
+        feat = self.model_func(images)
+        _, c = feat.size()
 
-        output = self.euclidean_dist(query_feat, proto_feat)
+        feat = feat.view(episode_size, self.way_num, self.shot_num + self.query_num, c)
+        targets = targets.view(episode_size, self.way_num, self.shot_num + self.query_num)
+
+        support_feat = feat[:, :, :self.shot_num, :].contiguous() \
+            .view(episode_size, self.way_num * self.shot_num, c)
+        query_feat = feat[:, :, self.shot_num:, :].contiguous() \
+            .view(episode_size, self.way_num * self.query_num, c)
+        query_targets = targets[:, :, self.shot_num:].contiguous().view(-1)
+
+        output = self.proto_layer(query_feat, support_feat) \
+            .view(episode_size * self.way_num * self.query_num, self.way_num)
         prec1, _ = accuracy(output, query_targets, topk=(1, 3))
+
         return output, prec1
 
     def set_forward_loss(self, batch):
@@ -34,28 +69,24 @@ class ProtoNet(MetricModel):
         :param batch:
         :return:
         """
-        support_images, support_targets, query_images, query_targets = \
-            self.progress_batch(batch)
+        images, targets = self.progress_batch2(batch)
+        episode_size = images.size(0) // (self.way_num * (self.shot_num + self.query_num))
 
-        query_feat = self.model_func(query_images)
-        support_feat = self.model_func(support_images)
-        proto_feat = torch.mean(support_feat.view(self.way_num, self.shot_num, -1), 1)
+        feat = self.model_func(images)
+        _, c = feat.size()
 
-        output = self.euclidean_dist(query_feat, proto_feat)
+        feat = feat.view(episode_size, self.way_num, self.shot_num + self.query_num, c)
+        targets = targets.view(episode_size, self.way_num, self.shot_num + self.query_num)
+
+        support_feat = feat[:, :, :self.shot_num, :].contiguous() \
+            .view(episode_size, self.way_num * self.shot_num, c)
+        query_feat = feat[:, :, self.shot_num:, :].contiguous() \
+            .view(episode_size, self.way_num * self.query_num, c)
+        query_targets = targets[:, :, self.shot_num:].contiguous().view(-1)
+
+        output = self.proto_layer(query_feat, support_feat) \
+            .view(episode_size * self.way_num * self.query_num, self.way_num)
         loss = self.loss_func(output, query_targets)
         prec1, _ = accuracy(output, query_targets, topk=(1, 3))
+
         return output, prec1, loss
-
-    def euclidean_dist(self, input1, input2):
-        """
-        calc euclidean distance for each of the input1 to input2
-        :param input1: [query_num * way_num, feat_dim]
-        :param input2: [way_num, feat_dim]
-        :return:
-        """
-        input1 = input1.unsqueeze(1)
-        input2 = input2.unsqueeze(0)
-
-        dist = -torch.sum(torch.pow(input1 - input2, 2), 2)
-
-        return dist
