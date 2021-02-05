06ec0a283b52ce999b7a56b5991748683dbcdfb5
LegenDong <dcq2254142563@gmail.com>
2021-02-05 15:41:18 +0800

    add rfs model and multi-task support

--- core/model/loss.py
+++ core/model/loss.py
@@ -23,6 +23,9 @@ class DistillKLLoss(nn.Module):
         self.T = T
 
     def forward(self, y_s, y_t):
+        if y_t is None:
+            return 0.
+
         p_s = F.log_softmax(y_s / self.T, dim=1)
         p_t = F.softmax(y_t / self.T, dim=1)
         loss = F.kl_div(p_s, p_t, size_average=False) * (self.T ** 2) / y_s.size(0)
