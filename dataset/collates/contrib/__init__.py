f057c96c5b4f12188e3a5c208c661b99d4e6f0eb
yangcedrus <yangcedrus@gmail.com>
2021-03-19 20:34:41 +0800

    fix

---
+++ dataset/collates/contrib/__init__.py
@@ -0,0 +1,21 @@
+from .autoaugment import ImageNetPolicy
+from .cutout import Cutout
+from .randaugment import RandAugment
+from torchvision import transforms
+CJ_DICT = {'brightness': 0.4, 'contrast': 0.4, 'saturation': 0.4}
+
+def get_augment_method(config,):
+    if 'augment_method' not in config or config['augment_method'] == 'NormalAug':
+        trfms = [transforms.ColorJitter(**CJ_DICT),
+                 transforms.RandomHorizontalFlip()]
+    elif config['augment_method'] == 'AutoAugment':
+        trfms = [ImageNetPolicy()]
+    elif config['augment_method'] == 'Cutout':
+        trfms = [Cutout()]
+    elif config['augment_method'] == 'RandAugment':
+        trfms = [RandAugment()]
+    elif config['augment_method'] == 'MTLAugment':
+        trfms = [transforms.CenterCrop(80),
+                 transforms.RandomHorizontalFlip(), ]
+
+    return trfms
