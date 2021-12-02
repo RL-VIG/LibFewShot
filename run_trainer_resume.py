5dbb88fabc7309776d924efb72ac3f7a8951bd17
wZuck <wangziyi@smail.nju.edu.cn>
2021-12-02 13:46:17 +0800

    fix ddp run_trainer_resume

--- run_trainer_resume.py
+++ run_trainer_resume.py
@@ -3,14 +3,24 @@ import sys
 
 sys.dont_write_bytecode = True
 
+import torch
 import os
-
 from core.config import Config
 from core import Trainer
 
-PATH = "./results/RFSModel-miniImageNet--ravi-resnet12-5-1-KD2-Aug-28-2021-14-47-07"
+PATH = "./results/Baseline-miniImageNet--ravi-Conv64F-5-5-Dec-01-2021-06-52-41"
+
+
+def main(rank, config):
+    trainer = Trainer(rank, config)
+    trainer.train_loop(rank)
+
 
 if __name__ == "__main__":
     config = Config(os.path.join(PATH, "config.yaml"), is_resume=True).get_config_dict()
-    trainer = Trainer(config)
-    trainer.train_loop()
+
+    if config["n_gpu"] > 1:
+        os.environ["CUDA_VISIBLE_DEVICES"] = config["device_ids"]
+        torch.multiprocessing.spawn(main, nprocs=config["n_gpu"], args=(config,))
+    else:
+        main(0, config)
