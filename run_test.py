6d8c694bf9f716a357d20b8e8303eb35f7fe72fd
VincenDen <dengwenxiao@localhost.localdomain>
2024-05-04 18:37:22 +0800

    add meta-baseline

--- run_test.py
+++ run_test.py
@@ -25,7 +25,7 @@ def main(rank, config):
 
 
 if __name__ == "__main__":
-    config = Config(os.path.join(PATH, "config.yaml")).get_config_dict()
+    config = Config(os.path.join(PATH, "config.yaml"), VAR_DICT).get_config_dict()
 
     if config["n_gpu"] > 1:
         os.environ["CUDA_VISIBLE_DEVICES"] = config["device_ids"]
