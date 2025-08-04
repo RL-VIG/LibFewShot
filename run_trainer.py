477836a679298343abc7e070b9858b12a07cc23e
cjn <jianuocao0105@gmail.com>
2025-08-04 14:09:41 -0700

    clean1

--- run_trainer.py
+++ run_trainer.py
@@ -15,8 +15,8 @@ def main(rank, config):
 
 
 if __name__ == "__main__":
-    # config = Config("./reproduce/Baseline/Baseline-miniImageNet--ravi-Conv64F-5-Reproduce.yaml").get_config_dict()
-    config = Config("./config/fgfl_cub_51.yaml").get_config_dict()
+    config = Config("./reproduce/Baseline/Baseline-miniImageNet--ravi-Conv64F-5-Reproduce.yaml").get_config_dict()
+
     if config["n_gpu"] > 1:
         os.environ["CUDA_VISIBLE_DEVICES"] = config["device_ids"]
         torch.multiprocessing.spawn(main, nprocs=config["n_gpu"], args=(config,))
